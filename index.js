cc092ba393751799ebd34cc8d2170aabdc510212
bren-n <geraldine.aillon1901@alumnos.ubiobio.cl>
2022-11-10 22:51:40 -0300

    Inicializacion proyecto

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+//const mongoose = require('mongoose');
+//const cors = require ('cors');
+const dotenv = require('dotenv');
+const { default: mongoose } = require('mongoose');
+const app = express();
+dotenv.config();
+
+const options={
+    useNewUrlParser: true,
+    autoIndex:true,
+    keepAlive:true,
+    connectTimeoutMS: 10000,
+    socketTimeoutMS: 45000,
+    family:4,
+    useUnifiedTopology: true
+
+}
+mongoose.connect(process.env.DB, options, (error)=>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Connected to Database');
+    }
+})
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server started on port ${process.env.PORT}`);
+
+
+})
+
