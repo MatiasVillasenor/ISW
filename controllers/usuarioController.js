ebcb59cf4c0b17729489607902d38f0f060da25b
Matías Villaseñor <104093393+MatiasVillasenor@users.noreply.github.com>
2022-11-14 13:51:57 -0300

    Update usuarioController.js

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,20 +2,20 @@ const usuario = require('../models/usuario');
 
 const createUsuario = (req,res)=>{
     const {name,foto,nroTelefono,role} =req.body
-    const newUsuario = new EspacioCom({
+    const newUsuario = new usuario({
         name,
         foto,
         nroTelefono,
         role
     })
-    newEspacioCom.save((error,espacioCom)=>{
+    newUsuario.save((error,usuario)=>{
         if(error){
-            return res.status(400).send({message:"No se ha podido crear usuario"});
+            return res.status(400).send({message:"No se ha podido crear el usuario"});
         }
-        return res.status(201).send(espacioCom);
+        return res.status(201).send(usuario);
     })
 }
 
 module.exports ={
-    createEspacioCom    
+    createusuario   
 }
