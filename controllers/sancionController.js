4fb5748312a92568cbd0c72752945f2e97c85de1
Matías Villaseñor <104093393+MatiasVillasenor@users.noreply.github.com>
2022-11-14 13:49:59 -0300

    Update sancionController.js

--- controllers/sancionController.js
+++ controllers/sancionController.js
@@ -1,15 +1,15 @@
 const sancion = require('../models/sancion');
 
 const createsancion = (req,res)=>{
-    const {tipo,tiempoSacnion,montoSancion} =req.body
+    const {tipo,tiempoSancion,montoSancion} =req.body
     const newSancion = new sancion({
         tipo,
-        tiempoSacnion,
+        tiempoSancion,
         montoSancion
     })
     newSancion.save((error,sancion)=>{
         if(error){
-            return res.status(400).send({message:"No se ha podido crear el espacio común"});
+            return res.status(400).send({message:"No se ha podido aplicar la sanción"});
         }
         return res.status(201).send(sancion);
     })
